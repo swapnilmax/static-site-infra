3262d6e2ae3b795d58eba4b9ea72cdc4b9ffd47e
swapnilmax <63826307+swapnilmax@users.noreply.github.com>
2026-02-22 01:57:29 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,10 +1,9 @@
 terraform {
-    backend "s3"{
-
-        bucket = "static-site-state-bucket-007"
-        key    = "static-site/terraform.tfstate"
-        region = "ap-south-1"
-        use_lockfile   = true
-        encrypt = true
-    }
+  backend "s3" {
+    bucket         = "static-site-state-bucket-007"
+    key            = "static-site/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-locks" 
+  }
 }
