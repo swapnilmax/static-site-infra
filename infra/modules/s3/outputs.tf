32eeb914fbdfaa6091a47a5cdc833cac5b845fef
swapnilmax <63826307+swapnilmax@users.noreply.github.com>
2026-02-22 01:09:32 +0530

    Update outputs.tf

--- infra/modules/s3/outputs.tf
+++ infra/modules/s3/outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_name" {
-    value = aws_s3_bucket.this.bucket
+    value = aws_s3_bucket.this.id
 }
 
 output "bucket_domain" {
-    value = aws_s3_bucket.this.bucket_domain_name
+    value = aws_s3_bucket.this.bucket_regional_domain_name
 }
