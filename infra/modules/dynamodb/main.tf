4698003ba88fedc066e542b8f37661813a567c45
swapnilmax <63826307+swapnilmax@users.noreply.github.com>
2026-02-22 01:59:03 +0530

    Update main.tf

--- infra/modules/dynamodb/main.tf
+++ infra/modules/dynamodb/main.tf
@@ -1,5 +1,5 @@
-resource "aws_dynamodb_table" "terraform_lock" {
-  name         = var.table_name
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = terraform_locks
   billing_mode = "PAY_PER_REQUEST"
 
   hash_key = "LockID"
